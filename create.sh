53967bdc0bf2501c3a7966d32b6a54893fe19b17
Tobias Rahloff <rahloff@gmail.com>
2020-01-13 13:37:12 +0100

    it's 2020, baby

--- create.sh
+++ create.sh
@@ -6,7 +6,17 @@ sudo apt-key add - < Release.key
 sudo apt-get update
 
 # install tools
-sudo apt-get -y install aptitude curl git sshuttle
+sudo apt-get -y install curl git sshuttle
+
+# Git
+git config --global user.email "trahloff@deloitte.de"
+git config --global user.name "Tobias Rahloff"
+sudo apt-get install libgnome-keyring-dev
+sudo apt-get install libsecret-1-0 libsecret-1-dev
+cd /usr/share/doc/git/contrib/credential/libsecret
+sudo make
+git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+
 
 # Sources for arc-theme
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
@@ -30,27 +40,7 @@ cp ./numix-cursor-theme/Numix-Light/ /usr/share/icons/Numix-Light
 sudo update-alternatives --install /usr/share/icons/default/index.theme x-cursor-theme /usr/share/icons/Numix/cursor.theme 55
 sudo update-alternatives --set x-cursor-theme /usr/share/icons/Numix/cursor.theme
 
-# install atom
-wget https://atom.io/download/deb -O atom.deb
-sudo dpkg -i atom.deb
-sudo apt-get install -f
-apm install angularjs atom-beautify autocomplete-angular-material block-comment-plus color-picker file-icons git-control highlight-selected jsonlint minimap pigments
-
-# install redshift
-sudo apt-get -y install redshift-gtk
-# add to autostart
-cat <<EOF > ~/.config/autostart/redshift-gtk.desktop
-[Desktop Entry]
-Type=Application
-Exec=redshift-gtk -l 49.4743:8.5347
-Hidden=false
-NoDisplay=false
-X-GNOME-Autostart-enabled=false
-Name[de_DE]=redshift
-Name=redshift
-Comment[de_DE]=
-Comment=
-EOF
+# @TODO: install VS Code
 
 # install Franz
 wget https://github.com/meetfranz/franz-app/releases/download/4.0.4/Franz-linux-x64-4.0.4.tgz -O Franz.tgz
@@ -108,36 +98,8 @@ Categories=GNOME;GTK;Utility;Development;
 MimeType=text/plain;
 EOF
 
-# install java
-sudo apt-get install -y openjdk-8-jre
-
-# install shutter
-sudo apt-get -y install shutter
-
-# add to autostart
-cat <<EOF > ~/.config/autostart/Franz.desktop
-[Desktop Entry]
-Version=1.0
-Name=Shutter
-Name[de_DE]=Shutter
-Name[pt_BR]=Shutter
-GenericName=Screenshot Tool
-GenericName[de_DE]=Anwendung für Bildschirmfotos
-GenericName[pt_BR]=Captura de tela
-Comment=Capture, edit and share screenshots
-Comment[de_DE]=Bildschirmfotos aufnehmen, bearbeiten und mit Anderen teilen
-Comment[pt_BR]=Aplicativo avançado para capturar imagens da tela
-Exec=shutter --min_at_startup
-Icon=shutter
-Terminal=false
-Type=Application
-Categories=Utility;Application;
-X-GNOME-Autostart-enabled=true
-Hidden=false
-EOF
-
 # install chromium
-sudo apt-get -y install chromium-browser
+sudo apt-get -y install chrome
 
 # install vlc
 sudo apt-get -y install vlc
@@ -158,17 +120,29 @@ sudo apt-get -y install docker-ce
 dconf write /org/gnome/shell/favorite-apps "['org.gnome.Nautilus.desktop', 'chromium-browser.desktop', 'franz.desktop', 'gnome-terminal.desktop', 'atom.desktop', 'postman.desktop']"
 
 # install fish
-sudo apt-get -y install fish
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt-get update
+sudo apt-get install fish
 chsh -s $(which fish)
+curl -L https://get.oh-my.fish | fish
+omf install bobthefish
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+sudo apt install fonts-powerline
+set -g theme_nerd_fonts yes
 
 # gimme dem reverse incremental searches, baby!
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-# install node
-git clone https://github.com/creationix/nvm.git ~/.nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
 
-nvm install --lts
+# Misc
+sudo apt install gnome-tweaks
+sudo apt install glances
+
+# CF CLI
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+sudo apt-get update
+sudo apt-get install cf-cli
